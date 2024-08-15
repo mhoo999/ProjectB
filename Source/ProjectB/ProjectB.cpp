e3a797d83270306c3eae260da5f4bbe631d2d4b7
myeonghoon <mhoo999@naver.com>
2024-08-15 11:54:46 +0900

    Initial commit

---
+++ Source/ProjectB/ProjectB.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectB, "ProjectB" );
